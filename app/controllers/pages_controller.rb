b61023c7ed4bf502f4e8359aa2c0205fe974ba4a
Chase Anderson <chase.17@gmail.com>
2017-03-05 17:44:57 +0000

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+    
+    #create method for home page
+    def home
+    end
+    
+    #create method for about page
+    def about
+    end
+    
+    
+end
